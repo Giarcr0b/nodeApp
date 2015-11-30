434ec1f8c1adac05ac11f5c4a474cc5c849f94a5
Giarcr0b <1109847@rgu.ac.uk>
2015-11-30 15:25:32 +0000

    First Line

--- twittertest.js
+++ twittertest.js
@@ -12,7 +12,7 @@ var client = new Twitter({
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
 
-client.get('search/tweets', {q: 'lolcat', count: '2'}, function(error, tweets){
+client.get('search/tweets', {q: 'lolcat', count: '10'}, function(error, tweets){
     console.log(tweets);
     response.end(JSON.stringify(tweets));
 });
